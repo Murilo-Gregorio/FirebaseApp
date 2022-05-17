a40f74988e707febaac585440518c0b6bacad616
murilo.sgregorio <murilosenacpi@gmail.com>
2022-05-17 20:45:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.firebaseapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_de_compras)
+
+        tratarLogin()
+    }
+
+    fun tratarLogin(){
+
+        if(FirebaseAuth.getInstance().currentUser != null){
+            Toast.makeText(this, "Logado", Toast.LENGTH_SHORT)
+        }else{
+            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+            val i = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+
+            startActivityForResult(i, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+        }else{
+            finishAffinity()
+        }
+    }
+}
